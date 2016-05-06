c8b481892f7300e166171c08494ddd4d9066a51f
Josh Rowe <josh@s-block.com>
2016-05-06 15:26:11 +0100

    change context processor path

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django_moj_template.context_processors.moj_context',
+                'moj_template.context_processors.moj_context',
             ],
         },
     },
