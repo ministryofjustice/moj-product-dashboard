612ea14c53ecfb2386f1c715003caee5e8e8db4c
cliff xuan <cliff.xuan@gmail.com>
2016-05-06 11:02:18 +0100

    STATIC_ROOT

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = location('static')
 STATIC_URL = '/static/'
 
 HEALTHCHECKS = [
