cc02e1809b55d603f880bc54775fbc04329beea1
James Narey <james.narey@digital.justice.gsi.gov.uk>
2016-05-10 16:30:19 +0100

    Integrate MoJ template

---
+++ dashboard/apps/prototype/context_processors.py
@@ -0,0 +1,11 @@
+def globals(request):
+
+    return {
+        'app_title': 'MoJ Product Dashboard', # Application Title (Populates <title>)
+        'proposition_title': 'MoJ Product Dashboard', # Proposition Title (Populates proposition header)
+        'phase': 'alpha', # Current Phase (Sets the current phase and the colour of phase tags). Presumed values: alpha, beta, live
+        'product_type': 'service', # Product Type (Adds class to body based on service type). Presumed values: information, service
+        'feedback_url': '', # Feedback URL (URL for feedback link in phase banner)
+    }
+
+# 'app_title' does not seem to passed to the template?
--- dashboard/settings.py
+++ dashboard/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'dashboard.apps.prototype.context_processors.globals',
             ],
         },
     },
@@ -124,6 +125,7 @@ USE_TZ = True
 
 STATIC_ROOT = location('static')
 STATIC_URL = '/static/'
+print(location('static'))
 
 HEALTHCHECKS = [
     'moj_irat.healthchecks.database_healthcheck',
