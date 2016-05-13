899c2e52d1ae870451671ab2496a977a5ae9ebc3
Josh Rowe <josh@s-block.com>
2016-05-13 15:12:36 +0100

    set env vars for db defaults for addition of template deploy

--- dashboard/settings/base.py
+++ dashboard/settings/base.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
-import dj_database_url
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,10 +83,13 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dashboard',
+        'NAME': os.environ.get('DB_NAME', 'dashboard'),
+        'USER': os.environ.get('DB_USERNAME', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
-DATABASES['default'].update(dj_database_url.config())
 
 
 # Password validation
