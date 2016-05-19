65c5512c1dc0ed5a8bc997eec40ff1f99f6c8725
Josh Rowe <josh@s-block.com>
2016-05-19 14:18:35 +0100

    add admin for all models

--- dashboard/apps/prototype/admin.py
+++ dashboard/apps/prototype/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from . models import *
-# Register your models here.
+from .models import Person, Role, Rate, PersonRole, Client, Project, Task
+
 
 admin.site.register(Person)
-admin.site.register(Project)
+admin.site.register(Role)
 admin.site.register(Rate)
+admin.site.register(PersonRole)
 admin.site.register(Client)
+admin.site.register(Project)
 admin.site.register(Task)
