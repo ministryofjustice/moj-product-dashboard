07e19672bc12816f37c397035ec5d9cc98ff08f5
cliff xuan <cliff.xuan@gmail.com>
2016-05-20 15:47:08 +0100

    removed reference to Role and PersonRole

--- dashboard/apps/prototype/admin.py
+++ dashboard/apps/prototype/admin.py
@@ -5,8 +5,6 @@ from . models import *
 
 admin.site.register(Person)
 admin.site.register(Project)
-admin.site.register(Role)
 admin.site.register(Rate)
-admin.site.register(PersonRole)
 admin.site.register(Client)
 admin.site.register(Task)
