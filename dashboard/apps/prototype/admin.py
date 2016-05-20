e326a5c26176498fa34ef736bc1bf11c0e581c71
Josh Rowe <josh@s-block.com>
2016-05-20 16:53:30 +0100

    update models for admin

--- dashboard/apps/prototype/admin.py
+++ dashboard/apps/prototype/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 
-from .models import Person, Role, Rate, PersonRole, Client, Project, Task
+from .models import Person, Rate, Client, Project, Task
 
 
 admin.site.register(Person)
-admin.site.register(Role)
 admin.site.register(Rate)
-admin.site.register(PersonRole)
 admin.site.register(Client)
 admin.site.register(Project)
 admin.site.register(Task)
