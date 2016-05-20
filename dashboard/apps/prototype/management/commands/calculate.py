ae8e9cbe65830e764e08f1b581607c30efbb7721
cliff xuan <cliff.xuan@gmail.com>
2016-05-20 21:59:24 +0100

    command for generating rates

--- dashboard/apps/prototype/management/commands/calculate.py
+++ dashboard/apps/prototype/management/commands/calculate.py
@@ -3,49 +3,14 @@
 """
 calculate
 """
-from datetime import datetime, date, timedelta
+from datetime import date, timedelta
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from django.db.models import Q
 
-from dashboard.apps.prototype.models import Task, Person, Project, Client
-
-
-def print_person(person, padding=''):
-    if person.is_contractor:
-        line = '{}, {} (contractor)'.format(
-            person.name, person.job_title,
-            person.job_title)
-    else:
-        line = '{}, {} (civil servant)'.format(
-            person.name, person.job_title,
-            person.job_title)
-    print('{}{}'.format(padding, line))
-
-
-def print_task(task, start_date, end_date, padding='  '):
-    lines = []
-    lines.append('task name: {}'.format(task.name or 'N/A'))
-    if task.project.is_billable:
-        lines.append('project: {}, area: {}'.format(
-            task.project, task.project.client.name))
-    else:
-        lines.append('project: {} (non-billable), area: {}'.format(
-            task.project, task.project.client.name))
-    lines.append('task start: {}, end: {}, total: {:.5f} working days'.format(
-        task.start_date, task.end_date, task.days))
-    time_spent = task.time_spent(start_date, end_date)
-    lines.append(
-        'time spent in this time frame: {:.5f} days'.format(time_spent))
-    for index, line in enumerate(lines):
-        if index == 0:
-            print('{}- {}'.format(padding, line))
-        else:
-            print('{}  {}'.format(padding, line))
-
-
-def valid_date(s):
-    return datetime.strptime(s, "%Y-%m-%d").date()
+from dashboard.apps.prototype.models import Task
+from .helpers import (valid_date, get_persons, get_areas, get_projects,
+                      print_person, print_task, logger)
 
 
 class Command(BaseCommand):
@@ -63,64 +28,14 @@ class Command(BaseCommand):
         parser.add_argument('-a', '--areas', nargs='*', type=str)
         parser.add_argument('-n', '--names', nargs='*', type=str)
 
-    @staticmethod
-    def _get_persons(names):
-        if not names:
-            print('people: all')
-            return []
-        query = Q()
-        for item in [Q(name__icontains=name) for name in names]:
-            query |= item
-        persons = Person.objects.filter(query)
-        if not persons:
-            raise CommandError(
-                'could not find any person with name(s) {}'.format(
-                    ','.join(names)))
-        print('people: {}'.format(', '.join([p.name for p in persons])))
-        return persons
-
-    @staticmethod
-    def _get_areas(names):
-        if not names:
-            print('areas: all')
-            return []
-        query = Q()
-        for item in [Q(name__icontains=name) for name in names]:
-            query |= item
-        areas = Client.objects.filter(query)
-        if not areas:
-            raise CommandError(
-                'could not find any area with name(s) {}'.format(
-                    ','.join(names)))
-        print('areas: {}'.format(', '.join([p.name for p in areas])))
-        return areas
-
-    @staticmethod
-    def _get_projects(names, areas):
-        if not names and not areas:
-            print('projects: all')
-            return []
-        filter_by_name = Q()
-        for item in [Q(name__icontains=name) for name in names]:
-            filter_by_name |= item
-        projects = Project.objects.filter(filter_by_name)
-        if areas:
-            projects = projects.filter(client__in=areas)
-        if not projects:
-            area_names = ','.join([area.name for area in areas]) or 'all'
-            raise CommandError(
-                ('could not find any project with name(s) {} and area(s) {}'
-                 ).format(','.join(names), area_names))
-        print('projects: {}'.format(', '.join([p.name for p in projects])))
-        return projects
-
     def handle(self, *args, **options):
         start_date = options['start_date']
         end_date = options['end_date']
-        print('time frame start: {} end : {}'.format(start_date, end_date))
-        persons = self._get_persons(options['names'])
-        areas = self._get_areas(options['areas'])
-        projects = self._get_projects(options['projects'] or [], areas)
+        logger.info('time frame start: {} end : {}'.format(
+            start_date, end_date))
+        persons = get_persons(options['names'])
+        areas = get_areas(options['areas'])
+        projects = get_projects(options['projects'] or [], areas)
 
         tasks = Task.objects.filter(
             Q(start_date__gte=start_date, start_date__lte=end_date) |
@@ -135,7 +50,7 @@ class Command(BaseCommand):
         for task in tasks:
             person_to_task.setdefault(task.person, []).append(task)
         for person in person_to_task:
-            print('-' * 20)
+            logger.info('-' * 20)
             print_person(person)
             for task in person_to_task[person]:
                 print_task(task, start_date, end_date)
---
+++ dashboard/libs/tests/test_rate_generator.py
@@ -0,0 +1,43 @@
+from datetime import date
+
+import pytest
+
+from dashboard.libs.rate_generator import get_reference_rate, gen_rates
+
+
+@pytest.mark.parametrize("job_title, is_contractor, expected", [
+    # match the job title 100%
+    ('Agile Coach', True, 750),
+    ('Agile Coach', False, 750),
+    ('Developer', True, 465),
+    ('Developer', False, 390),
+    ('Web Ops Developer', True, 609),
+    ('Web Ops Developer', False, 360),
+
+    # match the job title more than 60%
+    ('Python Developer', True, 465),
+    ('Ruby Developer', True, 465),
+    ('Junior Developer', True, 465),
+    ('Senior Developer', True, 465),
+    ('Front End Developer', True, 465),
+    ('Web Ops', True, 609),
+    ('Web Ops Engineer', True, 609),
+    ('Web-ops', True, 609),
+
+    # match the job title less than 60%
+    ('Common Doorway/Strategy Assistant', False, 375),
+    ('Head of Digital Communications', False, 375),
+    ('Executive Assistant', False, 375),
+])
+def test_get_reference_rate(job_title, is_contractor, expected):
+    assert get_reference_rate(job_title, is_contractor) == expected
+
+
+def test_gen_rates():
+    rates = gen_rates('2015-01-01', '2017-01-01', '2QS', 450, 500)
+    dates = ['2015-01-01', '2015-07-01',
+             '2016-01-01', '2016-07-01',
+             '2017-01-01']
+    assert [d.strftime('%Y-%m-%d') for d in rates.index.date] == dates
+    for val in rates.values:
+        assert 450 <= val <= 500
