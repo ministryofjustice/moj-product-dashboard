8b85951ec61ba49827d4b11e37d123380d2094b8
Josh Rowe <josh@s-block.com>
2016-05-25 15:27:07 +0100

    add rate converter with tests

---
+++ dashboard/libs/rate_converter.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from calendar import monthrange
+from datetime import datetime, date
+from decimal import Decimal
+
+from dateutil.rrule import rrule, MONTHLY
+from extended_choices import Choices
+import numpy as np
+
+from .date_tools import get_workdays
+
+
+RATE_TYPES = Choices(
+    ('DAY',  1, 'Dayly rate'),
+    ('MONTH',   2, 'Monthly salary'),
+    ('YEAR', 3, 'Yearly salary'),
+)
+
+
+def dec_workdays(start_date, end_date):
+    return Decimal(get_workdays(start_date, end_date))
+
+
+class RateConverter():
+    """
+    RateConverter converts yearly salary and monthly salary to day rate
+    """
+    def __init__(self, rate, rate_type=RATE_TYPES.DAY):
+        """
+        param: rate: Decimal object
+        param: rate_type: int object - one of RATE_TYPES
+        """
+        self.rate = rate
+        self.rate_type = rate_type
+
+    def _year_date_range(self, as_of):
+        year = as_of.year
+        return date(as_of.year, 1, 1), date(year, 12, 31)
+
+    def _month_date_range(self, as_of):
+        year = as_of.year
+        month = as_of.month
+        day, number = monthrange(year, month)
+        return date(year, month, 1), date(year, month, number)
+
+    def method_name(self, extension):
+        return '_{}_{}'.format(
+            RATE_TYPES.for_value(self.rate_type).constant.lower(),
+            extension)
+
+    @property
+    def get_date_range(self):
+        return getattr(self, self.method_name('date_range'))
+
+    def average_day_rate(self, start_date=None, end_date=None, as_of=None):
+        """
+        Returns an average day rate over a time period
+        #TODO: not implemented yet - returns day rate as of now()
+
+        param: start_date: date object - beginning of time period for average
+        param: end_date: date - object end of time period for average
+        return: Decimal object - average day rate
+        """
+        if self.rate_type is RATE_TYPES.DAY:
+            return self.rate
+
+        if not start_date or not end_date:
+            if not as_of:
+                as_of = datetime.now()
+            start_date, end_date = self.get_date_range(as_of)
+            # No point continuing although would be same result
+            return round(self.rate / dec_workdays(start_date, end_date), 2)
+
+        total_workdays = dec_workdays(start_date, end_date)
+
+        weights = []
+        rates = []
+
+        months = [dt.date() for dt in rrule(MONTHLY, dtstart=start_date, until=end_date)]
+        # include last month
+        months.append(date(end_date.year, end_date.month, 1))
+        for n, start in enumerate(months):
+            rate = self.rate / dec_workdays(*self.get_date_range(start))
+
+            day, number = monthrange(start.year, start.month)
+            end = date(start.year, start.month, number)
+
+            if n is 0:
+                start = start_date
+            elif n + 1 is len(months):
+                end = end_date
+
+            # This is a bit horrible
+            # numpy won't average decimals
+            weights.append(float(dec_workdays(start, end) / total_workdays))
+            rates.append(float(rate))
+
+        return round(Decimal(np.average(rates, weights=weights)), 2)
+
+
---
+++ dashboard/libs/tests/test_rate_converter.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from decimal import Decimal
+
+import pytest
+
+from ..rate_converter import RateConverter, RATE_TYPES
+
+
+@pytest.mark.parametrize('as_of, rate, rate_type, expected', [
+    (date(2016, 1, 1), '500', RATE_TYPES.DAY, '500'),  # Day rate
+    (date(2016, 1, 1), '4600', RATE_TYPES.MONTH, '230'),  # Jan / 20 days
+    (date(2016, 2, 1), '4600', RATE_TYPES.MONTH, '219.05'),  # Feb / 21 days
+    (date(2016, 3, 1), '4600', RATE_TYPES.MONTH, '219.05'),  # Mar / 21 days
+    (date(2016, 4, 1), '4600', RATE_TYPES.MONTH, '219.05'),  # Apr / 21 days
+    (date(2016, 5, 1), '4600', RATE_TYPES.MONTH, '230'),  # May / 20 days
+    (date(2016, 6, 1), '4600', RATE_TYPES.MONTH, '209.09'),  # Jun / 22 days
+    (date(2016, 7, 1), '4600', RATE_TYPES.MONTH, '219.05'),  # Jul / 21 days
+    (date(2016, 8, 1), '4600', RATE_TYPES.MONTH, '209.09'),  # Aug / 22 days
+    (date(2016, 9, 1), '4600', RATE_TYPES.MONTH, '209.09'),  # Sep / 22 days
+    (date(2016, 10, 1), '4600', RATE_TYPES.MONTH, '219.05'),  # Oct / 21 days
+    (date(2016, 11, 1), '4600', RATE_TYPES.MONTH, '209.09'),  # Nov / 22 days
+    (date(2016, 12, 1), '4600', RATE_TYPES.MONTH, '230'),  # Dec / 20 days
+    (date(2014, 1, 1), '60000', RATE_TYPES.YEAR, '237.15'),  # 2014
+    (date(2015, 1, 1), '60000', RATE_TYPES.YEAR, '237.15'),  # 2015
+    (date(2016, 1, 1), '60000', RATE_TYPES.YEAR, '237.15'),  # 2016
+])
+def test_rate_at_certain_time(as_of, rate, rate_type, expected):
+    converter = RateConverter(Decimal(rate), rate_type)
+
+    assert converter.average_day_rate(as_of=as_of) == Decimal(expected)
+
+
+@pytest.mark.parametrize('start_date, end_date, rate, rate_type, expected', [
+    (date(2016, 4, 28), date(2016, 4, 28), '500', RATE_TYPES.DAY, '500'),  # Day rate
+    (date(2016, 1, 1), date(2016, 1, 31), '4600', RATE_TYPES.MONTH, '230'),  # Jan / 20 days
+    (date(2016, 2, 1), date(2016, 2, 29), '4600', RATE_TYPES.MONTH, '219.05'),  # Feb / 21 days
+    (date(2016, 3, 1), date(2016, 3, 31), '4600', RATE_TYPES.MONTH, '219.05'),  # Mar / 21 days
+    (date(2016, 4, 1), date(2016, 4, 30), '4600', RATE_TYPES.MONTH, '219.05'),  # Apr / 21 days
+    (date(2016, 5, 1), date(2016, 5, 31), '4600', RATE_TYPES.MONTH, '230'),  # May / 20 days
+    (date(2016, 6, 1), date(2016, 6, 30), '4600', RATE_TYPES.MONTH, '209.09'),  # Jun / 22 days
+    (date(2016, 7, 1), date(2016, 7, 31), '4600', RATE_TYPES.MONTH, '219.05'),  # Jul / 21 days
+    (date(2016, 8, 1), date(2016, 8, 31), '4600', RATE_TYPES.MONTH, '209.09'),  # Aug / 22 days
+    (date(2016, 9, 1), date(2016, 9, 30), '4600', RATE_TYPES.MONTH, '209.09'),  # Sep / 22 days
+    (date(2016, 10, 1), date(2016, 10, 31), '4600', RATE_TYPES.MONTH, '219.05'),  # Oct / 21 days
+    (date(2016, 11, 1), date(2016, 11, 30), '4600', RATE_TYPES.MONTH, '209.09'),  # Nov / 22 days
+    (date(2016, 12, 1), date(2016, 12, 31), '4600', RATE_TYPES.MONTH, '230'),  # Dec / 20 days
+    (date(2014, 1, 1), date(2014, 12, 31), '60000', RATE_TYPES.YEAR, '237.15'),  # 2014
+    (date(2015, 1, 1), date(2015, 12, 31), '60000', RATE_TYPES.YEAR, '237.15'),  # 2015
+    (date(2016, 1, 1), date(2016, 12, 31), '60000', RATE_TYPES.YEAR, '237.15'),  # 2016
+
+    (date(2016, 1, 4), date(2016, 1, 4), '60000', RATE_TYPES.YEAR, '237.15'),  # One day
+    (date(2016, 1, 4), date(2016, 1, 4), '4600', RATE_TYPES.MONTH, '230'),  # One day
+
+    (date(2016, 1, 28), date(2016, 2, 2), '4600', RATE_TYPES.MONTH, '224.52'),  # Equal days per month
+    (date(2016, 1, 28), date(2016, 2, 3), '4600', RATE_TYPES.MONTH, '223.43'),  # different amount of days in month
+    (date(2016, 1, 28), date(2016, 6, 3), '4600', RATE_TYPES.MONTH, '219.30'),  # multiple months
+    (date(2014, 1, 28), date(2016, 2, 3), '4600', RATE_TYPES.MONTH, '216.36'),  # multiple years
+])
+def test_cross_month_average(start_date, end_date, rate, rate_type, expected):
+    converter = RateConverter(Decimal(rate), rate_type)
+
+    assert converter.average_day_rate(
+        start_date=start_date,
+        end_date=end_date) == Decimal(expected)
