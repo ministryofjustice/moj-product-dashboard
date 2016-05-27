f50b519901cb4bb5517504b0ec993ac4fa404d3b
cliff xuan <cliff.xuan@gmail.com>
2016-05-27 14:41:36 +0100

    dropdown project selector

--- dashboard/apps/prototype/views.py
+++ dashboard/apps/prototype/views.py
@@ -2,14 +2,14 @@ import datetime
 import json
 from dateutil import relativedelta
 import random
-from django.shortcuts import render
+from collections import OrderedDict
 
+from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponseBadRequest
-
+from django.http.response import HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required
 
 from .models import Person, Project, Client, Task, Rate
-
 from dashboard.libs.figure_gen import get_figure
 
 
@@ -33,8 +33,24 @@ def get_total_times(projects):
 
 @login_required
 def index(request):
-
-    return render(request, 'index.html')
+    try:
+        project_id = request.GET['projectid']
+    except KeyError:
+        project_id = Project.objects.first().id
+        return redirect('/?projectid={}'.format(project_id))
+    try:
+        project_id = int(project_id)
+    except ValueError:
+        # TODO better error page
+        return HttpResponseBadRequest(
+            'invalid projectid "{}"'.format(project_id))
+    areas = {
+        area.name: {p.id: p.name for p in area.projects.all()}
+        for area in Client.objects.all()
+    }
+    areas = OrderedDict(sorted([(k, v) for k, v in areas.items() if v]))
+    context = {'areas': areas,  'project_id': project_id}
+    return render(request, 'index.html', context)
 
 
 def simple(request):
