4915b1968f71eed094bc75b3358af97999caec74
Josh Rowe <josh@s-block.com>
2016-05-27 12:46:42 +0100

    add search fields to admin

--- dashboard/apps/prototype/admin.py
+++ dashboard/apps/prototype/admin.py
@@ -18,6 +18,7 @@ class PersonAdmin(ReadOnlyAdmin):
     readonly_fields = ('avatar_tag', )
     list_display = ('avatar_tag', 'name', 'job_title', 'is_contractor',
                     'is_current')
+    search_fields = ('name', 'job_title')
 
     def avatar_tag(self, obj):
         return '<img src="{}" style="height:40px;"/>'.format(obj.avatar)
@@ -26,11 +27,23 @@ class PersonAdmin(ReadOnlyAdmin):
 
 
 class RateAdmin(FinancePermissions, admin.ModelAdmin):
-    pass
+    search_fields = ('person__name', 'person__job_title')
+
+
+class ClientAdmin(ReadOnlyAdmin):
+    search_fields = ('name', 'float_id')
+
+
+class ProjectAdmin(ReadOnlyAdmin):
+    search_fields = ('name', 'float_id')
+
+
+class TaskAdmin(ReadOnlyAdmin):
+    search_fields = ('name', 'person__name', 'project__name', 'float_id')
 
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Rate, RateAdmin)
-admin.site.register(Client, ReadOnlyAdmin)
-admin.site.register(Project, ReadOnlyAdmin)
-admin.site.register(Task, ReadOnlyAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
