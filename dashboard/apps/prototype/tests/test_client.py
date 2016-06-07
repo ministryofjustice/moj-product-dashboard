ed0b78fc61d7d065d232a691c38b9e543b21dd02
cliff xuan <cliff.xuan@gmail.com>
2016-06-07 10:28:36 +0100

    compare by project.id

--- dashboard/apps/prototype/tests/test_client.py
+++ dashboard/apps/prototype/tests/test_client.py
@@ -10,6 +10,5 @@ def test_client():
     client = mommy.make(Client)
     projects = [mommy.make(Project, client=client) for _ in range(10)]
     assert str(client) == client.name
-    sorted_names = sorted([p.name for p in projects])
-    # can only compare by name as the id of the objects will be different
-    assert [p.name for p in client.projects.order_by('name')] == sorted_names
+    sorted_pids = sorted([p.id for p in projects])
+    assert [p.id for p in client.projects.order_by('id')] == sorted_pids
