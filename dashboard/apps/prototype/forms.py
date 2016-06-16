db52cb7860bfb3cc50f1efc92fb6e4451b477e20
Josh Rowe <josh@s-block.com>
2016-06-16 12:09:04 +0100

    create form for payroll upload

---
+++ dashboard/apps/prototype/forms.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from datetime import date, datetime, time
+
+from django import forms
+from django.conf import settings
+from django.utils import timezone
+
+from .widgets import MonthYearWidget
+
+
+def year_range(backward=0, forward=10):
+    this_year = date.today().year
+    return range(this_year-backward, this_year+forward)
+
+
+class ConvertDateMixin(object):
+    def _convert_date(self, d):
+        d = datetime.combine(d, time(hour=0, minute=0))
+        d = timezone.make_aware(d, timezone.get_current_timezone())
+        return d
+
+
+class PayrollUploadForm(forms.Form, ConvertDateMixin):
+    payroll_file = forms.FileField()
+    date = forms.DateField(widget=MonthYearWidget(
+        years=year_range(backward=4, forward=3)
+    ))
+
+    @property
+    def month(self):
+        return self._convert_date(self.cleaned_data['date'])
+
