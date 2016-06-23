7e754f9f7ba6189fa0a7a859928ef83de72c8e4e
Josh Rowe <josh@s-block.com>
2016-06-23 17:10:43 +0100

    allow for errors matching names in payroll upload  - don't save any rates if there are any errors in the upload

--- dashboard/apps/prototype/forms.py
+++ dashboard/apps/prototype/forms.py
@@ -27,48 +27,77 @@ class ConvertDateMixin(object):
 
 
 class PayrollUploadForm(forms.Form, ConvertDateMixin):
-    payroll_file = forms.FileField(required=True)
     date = forms.DateField(required=True, widget=MonthYearWidget(
         years=year_range(backward=4, forward=3)
     ))
+    payroll_file = forms.FileField(required=True)
 
     @property
     def month(self):
         return self.cleaned_data['date'].strftime('%Y-%m')
 
-    def process_upload(self):
+    def get_person(self, row, data):
+        try:
+            return Person.objects.get(
+                name__icontains=data.get('Surname'),
+                name__startswith=data.get('Init')[0],
+                is_contractor=False,
+            )
+        except Person.DoesNotExist:
+            try:
+                return Person.objects.get(
+                    name__icontains=data.get('Surname'),
+                    is_contractor=False,
+                )
+            except (Person.DoesNotExist, Person.MultipleObjectsReturned):
+                pass
+            self.add_error(
+                'payroll_file',
+                'ERROR ROW %s: Civil Servant not found with Surname "%s" '
+                'and initials "%s"' %
+                (row, data.get('Surname'), data.get('Init')))
+        except Person.MultipleObjectsReturned:
+            self.add_error(
+                'payroll_file',
+                'ERROR ROW %s: Multiple Civil Servants found with Surname '
+                '"%s"' % (row, data.get('Surname')))
+
+    def clean_payroll_file(self):
         start = self.cleaned_data['date']
         workbook = xlrd.open_workbook(
             file_contents=self.cleaned_data['payroll_file'].read())
 
         worksheet = workbook.sheet_by_index(0)
 
-        headers = worksheet.row_values(1)
-        for row in range(2, worksheet.nrows):
+        headers = worksheet.row_values(6)
+
+        payroll = []
+
+        for row in range(7, worksheet.nrows):
             row_data = worksheet.row_values(row)
             if row_data[0]:
                 data = dict(zip(headers, row_data))
-                try:
-                    person = Person.objects.get(
-                        name__icontains=data.get('Surname'))
-                except Person.DoesNotExist:
-                    self.add_error(
-                        None,
-                        'ERROR ROW %s: Person not found with Surname "%s"' %
-                        (row, data.get('Surname')))
-                except Person.MultipleObjectsReturned:
-                    self.add_error(
-                        None,
-                        'ERROR ROW %s: Multiple people found with Surname "%s"' %
-                        (row, data.get('Surname')))
-                else:
+                person = self.get_person(row, data)
+                if person:
                     day_rate = Decimal(data['Total']) / get_workdays(
                         start, start + relativedelta(day=31))
 
-                    rate = Rate.objects.create(
-                        rate=day_rate,
-                        person=person,
-                        start_date=start,
-                    )
-            else:
-                break
+                    payroll.append({
+                        'person': person,
+                        'rate': day_rate,
+                        'start': start,
+                    })
+
+        return payroll
+
+    def process_upload(self):
+        for pay in self.cleaned_data['payroll_file']:
+            created, rate = Rate.objects.get_or_create(
+                person=pay['person'],
+                start_date=pay['start'],
+                defaults=dict(rate=pay['rate'],)
+            )
+            if not created:
+                rate.rate = pay['rate']
+                rate.save()
+
