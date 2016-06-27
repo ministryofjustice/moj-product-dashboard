f4f876f192c27298cd49573f6f34af5ec80cf7f5
Josh Rowe <josh@s-block.com>
2016-06-27 12:21:26 +0100

    add test for unvalis payroll upload

--- dashboard/apps/prototype/tests/test_payroll_upload.py
+++ dashboard/apps/prototype/tests/test_payroll_upload.py
@@ -43,3 +43,28 @@ def test_valid_upload_form():
     assert form.month == '2016-01'
     assert form.save() is None
     assert form.save() is None
+
+
+@pytest.mark.django_db
+def test_invalid_upload_form():
+    mommy.make(Person, name='AD Surname1', is_contractor=False)
+    mommy.make(Person, name='AB Surname1', is_contractor=False)
+    mommy.make(Person, name='B Surname2', is_contractor=False)
+    mommy.make(Person, name='X Surname3', is_contractor=False)
+    mommy.make(Person, name='P Surname3', is_contractor=False)
+
+    fb = open(abspath(join(dirname(__file__), 'data/payroll_test.xls')), 'rb')
+    form = PayrollUploadForm(
+        data={'date': date(2016, 1, 1)},
+        files=MultiValueDict(
+            {'payroll_file': [SimpleUploadedFile(fb.name, fb.read())]}
+        ),
+    )
+
+    assert form.is_valid() is False
+    assert form.errors == {
+        'payroll_file': ['ERROR ROW 7: Multiple Civil Servants found with '
+                         'Surname "SURNAME1"',
+                         'ERROR ROW 12: Civil Servant not found with Surname '
+                         '"SURNAME3" and initials "CN"']
+    }
