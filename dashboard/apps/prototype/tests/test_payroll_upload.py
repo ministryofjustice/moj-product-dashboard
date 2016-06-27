4de2b3fb0cb9fe2f82497ed1d7b9ec256fd424a5
Josh Rowe <josh@s-block.com>
2016-06-27 15:37:20 +0100

    test payroll upload views

--- dashboard/apps/prototype/tests/test_payroll_upload.py
+++ dashboard/apps/prototype/tests/test_payroll_upload.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 from os.path import dirname, abspath, join
 
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import TestCase
 from django.utils.datastructures import MultiValueDict
 
 from model_mommy import mommy
@@ -68,3 +69,39 @@ def test_invalid_upload_form():
                          'ERROR ROW 12: Civil Servant not found with Surname '
                          '"SURNAME3" and initials "CN"']
     }
+
+
+class PayrollUploadTestCase(TestCase):
+    fixtures = ['auth_group_permissions.yaml', 'test_users']
+
+    def test_view_post_with_error(self):
+        fb = open(abspath(join(dirname(__file__), 'data/payroll_test.xls')), 'rb')
+        self.client.login(username='test_finance', password='Admin123')
+        self.client.get("/admin/prototype/person/upload/")
+        response = self.client.post(
+            '/admin/prototype/person/upload/',
+            {'date': date(2016, 1, 1), 'payroll_file': fb})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('ERROR' in str(response.content))
+
+    def test_view_post_no_error(self):
+        fb = open(abspath(join(dirname(__file__), 'data/payroll_test.xls')), 'rb')
+        self.client.login(username='test_finance', password='Admin123')
+        self.client.get("/admin/prototype/person/upload/")
+        mommy.make(Person, name='X Surname1', is_contractor=False)
+        mommy.make(Person, name='B Surname2', is_contractor=False)
+        mommy.make(Person, name='C Surname3', is_contractor=False)
+        mommy.make(Person, name='C Surname3', is_contractor=True)
+        response = self.client.post(
+            '/admin/prototype/person/upload/',
+            {'date': date(2016, 1, 1), 'payroll_file': fb})
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('ERROR' not in str(response.content))
+
+    def test_cant_access_upload(self):
+        fb = open(abspath(join(dirname(__file__), 'data/payroll_test.xls')), 'rb')
+        self.client.login(username='admin', password='Admin123')
+        response = self.client.post(
+            '/admin/prototype/person/upload/',
+            {'date': date(2016, 1, 1), 'payroll_file': fb})
+        self.assertEqual(response.status_code, 403)
