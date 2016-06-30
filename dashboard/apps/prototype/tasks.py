303b8a222efc63c5990695aa8ccfd75728c4ba67
Josh Rowe <josh@s-block.com>
2016-06-30 15:39:30 +0100

    cache after running float sync

--- dashboard/apps/prototype/tasks.py
+++ dashboard/apps/prototype/tasks.py
@@ -4,9 +4,16 @@ from datetime import timedelta, date
 from django.core.management import call_command
 
 from celery.task import periodic_task
+from celery import shared_task
 
 
-@periodic_task(run_every=timedelta(minutes=5))
+@periodic_task(run_every=timedelta(minutes=1))
 def sync_float():
     ninty_days_ago = date.today() - timedelta(days=90)
     call_command('sync', s=ninty_days_ago.strftime('%Y-%m-%d'))
+    cache.delay()
+
+
+@shared_task()
+def cache():
+    call_command('cache', 'gen')
