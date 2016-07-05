aad0dcef10c861a6baaf2857db0a716a1fcd435b
cliff xuan <cliff.xuan@gmail.seeohem>
2016-07-05 20:17:22 +0100

    use the flag `ignore_cache` when generating cache

--- dashboard/apps/prototype/management/commands/cache.py
+++ dashboard/apps/prototype/management/commands/cache.py
@@ -9,6 +9,7 @@ from django.core.management.base import BaseCommand
 from django.core.cache import cache
 
 from dashboard.apps.prototype.models import Project
+from dashboard.libs.date_tools import slice_time_window
 from .helpers import logger
 
 
@@ -25,10 +26,32 @@ class Command(BaseCommand):
         """
         generate cache
         """
-        for project in Project.objects.all():
+        for project in Project.objects.visible():
             logger.info('- generating caching for project "%s"', project)
             logger.info('  * spendings each month')
             project.profile(freq='MS')
+            if not project.first_task:
+                # no task no cost
+                continue
+            start_date = project.first_task.start_date
+            end_date = project.last_task.end_date
+            # monthly people costs
+            time_windows = slice_time_window(start_date, end_date, freq='MS')
+            # to date people costs
+            time_windows.append((start_date, date.today()))
+            for sdate, edate in time_windows:
+                project.people_costs(
+                    sdate, edate,
+                    ignore_cache=True)
+                project.people_costs(
+                    sdate, edate,
+                    contractor_only=True,
+                    ignore_cache=True)
+                project.people_costs(
+                    sdate, edate,
+                    non_contractor_only=True,
+                    ignore_cache=True)
+
             logger.info('  * spendings for entire project life span')
             project.profile()
             logger.info('  * spendings to date')
