8f061acd9c5ec8a00c1acf34b03ca05788210b5c
Jim Narey <jimnarey@me.com>
2016-07-27 15:33:10 +0100

    Working filter drop-down

--- dashboard/apps/prototype_admin/static/prototype_admin/js/dropdown_filter.js
+++ dashboard/apps/prototype_admin/static/prototype_admin/js/dropdown_filter.js
@@ -1,20 +1,40 @@
+let listVisible = false;
+let listFilter = document.getElementById("changelist-filter");
+
+
+function shrink(listFilterElement) {
+  listFilterElement.setAttribute("style", "height:54px");
+  listFilterElement.style.overflow = "hidden";
+  // visibilityButton.innerHTML = 'show';
+  listVisible = false;
+}
+
+function expand(listFilterElement) {
+  listFilterElement.setAttribute("style", "height:auto");
+  listFilterElement.style.overflow = "visible";
+  // visibilityButton.innerHTML = 'hide';
+  listVisible = true;
+}
+
+
 function dropDownClick() {
+  if (listVisible) {
+    shrink(listFilter);
 
+  } else if (!listVisible) {
+    expand(listFilter);
+
+  }
 }
-
-function shrink(element) {
-  
-}
-
-listFilter = document.getElementById("changelist-filter");
 
 if (listFilter != null) {
 
   filterHeading = listFilter.getElementsByTagName("h2")[0];
   headingText = filterHeading.innerHTML;
-  headingText = headingText + "<a id='dropdown_button'> X </a>";
+  headingText = headingText + "<a id='dropdown_button'>X</a>";
   filterHeading.innerHTML = headingText;
   dropDownButton = document.getElementById("dropdown_button");
   dropDownButton.onclick = dropDownClick;
+  shrink(listFilter);
 
 }
