9b63aa17af9685957aad591aa4de3fa2b9698db8
Josh Rowe <josh@s-block.com>
2016-08-02 12:43:36 +0100

    add test for cost.rate_between

--- dashboard/apps/prototype/tests/test_costs.py
+++ dashboard/apps/prototype/tests/test_costs.py
@@ -24,3 +24,31 @@ class CostTestCase(TestCase):
         self.assertEqual(
             cost.cost_between(date(2015, 1, 2), date(2015, 1, 3)),
             Decimal('0'))
+
+    def test_cost_inside_range(self):
+        project = mommy.make(Project)
+        cost = mommy.make(
+            Cost,
+            project=project,
+            start_date=date(2015, 1, 1),
+            type=COST_TYPES.ONE_OFF,
+            cost=Decimal('50')
+        )
+
+        self.assertEqual(
+            cost.cost_between(date(2015, 1, 1), date(2015, 1, 2)),
+            Decimal('50'))
+
+    def test_rate_between(self):
+        project = mommy.make(Project)
+        cost = mommy.make(
+            Cost,
+            project=project,
+            start_date=date(2015, 1, 1),
+            type=COST_TYPES.MONTHLY,
+            cost=Decimal('3000')
+        )
+
+        self.assertEqual(
+            round(cost.rate_between(date(2015, 1, 1), date(2015, 1, 2)), 2),
+            Decimal('142.86'))
