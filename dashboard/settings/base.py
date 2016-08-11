a5251e8068cfda5a69511ad773c61e0fc5c6a330
Josh Rowe <josh@s-block.com>
2016-08-11 12:17:42 +0100

    delete unused middleware

--- dashboard/settings/base.py
+++ dashboard/settings/base.py
@@ -61,7 +61,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'dashboard.apps.moj_admin.intercept.InterceptMiddleware',
 
 ]
 
