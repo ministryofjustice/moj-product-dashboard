188f00135aefe10a7a180a29017131a99be1ad51
Josh Rowe <josh@s-block.com>
2016-08-11 14:01:20 +0100

    remove prototype_admin app

--- dashboard/settings/base.py
+++ dashboard/settings/base.py
@@ -37,7 +37,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'prototype_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
