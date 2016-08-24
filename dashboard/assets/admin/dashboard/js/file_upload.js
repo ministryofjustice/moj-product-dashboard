f20042d9d1c4f7857bc9b7f2b5cc9021d4e279dc
axemonkey <hi+github@clivemurray.com>
2016-08-24 17:19:17 +0100

    WIP file upload bollocks

---
+++ dashboard/assets/admin/dashboard/js/file_upload.js
@@ -0,0 +1,88 @@
+'use strict';
+
+window.dashboard = window.dashboard || {};
+
+window.dashboard.fileUpload = {
+  init: function() {
+    var self = this;
+
+    Dropzone.autoDiscover = false;
+
+    if($('#dropzone-box').length) {
+      self.setup($('#dropzone-box'));
+    }
+  },
+
+  setup: function($el) {
+    var self = this;
+
+    $el.dropzone({
+      url: '.',
+      autoProcessQueue: false,
+      uploadMultiple: true,
+      maxFiles: 1,
+      clickable: '.dz-clickable',
+      acceptedFiles: '.xls',
+      paramName: 'payroll_file',
+      previewTemplate: document.getElementById('preview-template').innerHTML,
+      headers: { 'X-CSRF-Token' : $('input[name="csrfmiddlewaretoken"]').val() },
+
+      forceFallback: false
+    });
+
+    self.bindEvents($el);
+  },
+
+  bindEvents: function($el) {
+    var self = this;
+
+    $el.find('button').on('click', function(e) {
+      e.preventDefault();
+    });
+
+    $(document).on('click', '.js-remove-button', function(e) {
+      e.preventDefault();
+      Dropzone.forElement('#dropzone-box').removeAllFiles();
+      self.enableDropzone();
+    });
+    $el.on('dragenter', function(e) {
+      $('#dropzone-box').addClass('over');
+    });
+    $el.on('dragend', function(e) {
+      $('#dropzone-box').removeClass('over');
+    });
+
+    $('#dropzone-box').attrchange({
+      trackValues: true,
+      callback: function(e) {
+        if(e.attributeName === 'class') {
+          if(e.newValue.search(/dz-max-files-reached/i) == -1) {
+            self.enableDropzone();
+          } else {
+            self.disableDropzone();
+          }
+        }
+      }
+    });
+
+    $el.closest('form').on('submit', function(e) {
+      e.preventDefault();
+      Dropzone.forElement('#dropzone-box').processQueue();
+    });
+  },
+
+  enableDropzone: function() {
+    $('#dropzone-box').removeClass('over').removeClass('dropped');
+    $('.dropzone-message').show();
+  },
+
+  disableDropzone: function() {
+    $('#dropzone-box').removeClass('over').addClass('dropped');
+    $('.dropzone-message').hide();
+  }
+};
+
+
+$(function() {
+  window.dashboard.fileUpload.init();
+});
