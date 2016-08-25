000c509e4a96e49b6d0fabcd1fe4b06d85fbd5c1
Josh Rowe <josh@s-block.com>
2016-08-25 14:26:09 +0100

    separate send email to function

--- dashboard/apps/dashboard_auth/models.py
+++ dashboard/apps/dashboard_auth/models.py
@@ -19,27 +19,31 @@ class DashboardUser(User):
         return self.groups.filter(name=settings.FINANCE_GROUP_NAME).exists()
 
 
+def send_emails_to_finance(instance):
+    emails = [u.email for u in
+              User.objects.filter(~Q(pk=instance.pk)).filter(
+                  groups__name='Finance',
+                  email__isnull=False,) if u.email]
+
+    if emails:
+        template = get_template('email/finance_user_added.txt')
+        html = template.render({
+            'user': instance,
+        })
+        text = strip_tags(html)
+
+        send_mail(
+            'Finance User Added: %s' % instance.email,
+            text,
+            settings.DEFAULT_FROM_EMAIL,
+            emails,
+            html_message=html
+        )
+
+
 @receiver(m2m_changed, sender=User.groups.through)
 def send_finance_user_email(instance, pk_set, action, model, **kwargs):
     finance_group = model.objects.get(name=settings.FINANCE_GROUP_NAME)
 
     if action == 'post_add' and finance_group.pk in pk_set:
-        emails = [u.email for u in
-                  User.objects.filter(~Q(pk=instance.pk)).filter(
-                      groups__name='Finance',
-                      email__isnull=False,) if u.email]
-
-        if emails:
-            template = get_template('email/finance_user_added.txt')
-            html = template.render({
-                'user': instance,
-            })
-            text = strip_tags(html)
-
-            send_mail(
-                'Finance User Added: %s' % instance.email,
-                text,
-                settings.DEFAULT_FROM_EMAIL,
-                emails,
-                html_message=html
-            )
+        send_emails_to_finance(instance)
