5f92d183281fdecce69c1c897c3f8a67b3df29d4
Josh Rowe <josh@s-block.com>
2016-09-06 10:35:10 +0100

    Set our own date input format and date format in settings

--- dashboard/settings/base.py
+++ dashboard/settings/base.py
@@ -154,9 +154,15 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
+
+DATE_FORMAT = 'd/m/Y'
+
+DATE_INPUT_FORMATS = [
+    '%d/%m/%Y', '%Y-%m-%d'
+]
 
 
 # Static files (CSS, JavaScript, Images)
