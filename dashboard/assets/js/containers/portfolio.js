5ab7837dacd0c7a542f9b774251f390ffcd50683
Josh Rowe <josh@s-block.com>
2016-09-26 17:35:42 +0100

    add export button

--- dashboard/assets/js/containers/portfolio.js
+++ dashboard/assets/js/containers/portfolio.js
@@ -44,6 +44,9 @@ export class PortfolioContainer extends Component {
             MoJ Digital Portfolio
           </h1>
         </div>
+        <div className="projects-export">
+          <button className="button">EXPORT PROJECTS</button>
+        </div>
         <ProductTable
           products={this.state.products}
           showService={true}
