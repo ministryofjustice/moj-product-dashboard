5394445b502a6dd3a6636aa359badc143c1a7593
Josh Rowe <josh@s-block.com>
2016-11-09 17:51:46 +0000

    fix uwsgi command

--- docker/run.sh
+++ docker/run.sh
@@ -8,4 +8,4 @@ set -e
 /usr/bin/python3 manage.py loaddata auth_group_permissions
 
 # Run server
-/usr/local/bin/uwsgi --ini --http 0.0.0.0:$1 /app/conf/uwsgi.ini
+/usr/local/bin/uwsgi --http 0.0.0.0:$1 --ini /app/conf/uwsgi.ini
