509d2c39ff72ecebc863b66ed2cff65ea503e5dd
Josh Rowe <josh@s-block.com>
2016-11-09 17:00:46 +0000

    don't fake initial migration

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Run migrations if there are any
-/usr/bin/python3 manage.py migrate --fake-initial --run-syncdb --settings dashboard.settings.base --noinput
+/usr/bin/python3 manage.py migrate --run-syncdb --settings dashboard.settings.base --noinput
 
 # Load fixtures
 /usr/bin/python3 manage.py loaddata auth_group_permissions
