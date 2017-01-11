be4f7b889b61f334314c9a8407f25d441d292afe
Josh Rowe <josh@s-block.com>
2017-01-11 15:07:09 +0000

    Test product costs from fixture with actual data

--- dashboard/apps/dashboard/tests/test_product_costs.py
+++ dashboard/apps/dashboard/tests/test_product_costs.py
@@ -22,6 +22,11 @@ non_contractor_rate_two = Decimal('130')
 
 
 class ProductCostTestCase(TestCase):
+    fixtures = ['test_product_costs.yaml']
+
+    def assertDecimalEqual(self, a, b):
+        self.assertEqual(round(a, 2), round(b, 2))
+
     def setUp(self):
         self.product1 = mommy.make(Product)
         self.product2 = mommy.make(Product)
@@ -188,3 +193,70 @@ class ProductCostTestCase(TestCase):
         self.assertEqual(
             self.product1.total_cost,
             Decimal('387.75'))
+
+    def test_product_costs_from_fixture(self):
+        """
+        fixture loaded
+
+        month 10: (21 working days)
+
+        days = 13 (one in 5th task)
+        rate =  310
+        aditional_costs = 80
+        aditional_rate = 80/21
+
+        total = 13 * (310 + 80/21)
+        Decimal('4079.52380952380953')
+
+
+        month 11: (22 working days)
+
+        days = 10 (one in 8th task)
+        rate = 290
+        aditional_costs = 80
+        aditional_rate = 80/22
+
+        total = 10 * (290 + 80/22)
+        Decimal('2936.3636363636364')
+
+
+        month 12: (20 working days)
+
+        days = 10
+        rate = 300
+        aditional_costs = 80
+        aditional_rate = 80/20
+
+        total = 12 * (300 + 80/20)
+        Decimal('3040')
+
+
+
+        total:
+
+        days = 33
+
+        Decimal('10055.88744588744593')
+
+        """
+        product = Product.objects.get(pk=79)
+
+        self.assertDecimalEqual(
+            product.stats_between(
+                date(2016, 10, 1), date(2016, 10, 31)
+            )['total'],
+            Decimal('4079.52380952380953'))
+
+        self.assertDecimalEqual(
+            product.stats_between(
+                date(2016, 11, 1), date(2016, 11, 30)
+            )['total'],
+            Decimal('2936.3636363636364'))
+
+        self.assertDecimalEqual(
+            product.stats_between(
+                date(2016, 12, 1), date(2016, 12, 31)
+            )['total'],
+            Decimal('3040.00'))
+
+        self.assertDecimalEqual(product.total_cost, Decimal('10055.90'))
