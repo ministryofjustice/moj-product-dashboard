9c8edb226f83bb4d8081ad2c118d00c496e4d3ab
Cliff Xuan <cliff.xuan@gmail.com>
2017-03-31 16:21:29 +0100

    more caching

--- dashboard/apps/dashboard/management/commands/cache.py
+++ dashboard/apps/dashboard/management/commands/cache.py
@@ -10,6 +10,7 @@ from django.core.cache import cache
 from django.conf import settings
 
 from dashboard.apps.dashboard.models import Product
+from .helpers import contains_any
 
 
 class Command(BaseCommand):
@@ -20,17 +21,37 @@ class Command(BaseCommand):
             choices=['gen', 'rm'],
             help='generate or remove cache'
         )
+        parser.add_argument('-p', '--products', nargs='*', type=str)
 
-    def generate(self):
+    def generate(self, products):
         """
         generate cache
+        :param products: list of product objects
         """
-        for product in Product.objects.visible():
+        for product in products:
             logging.info('- generating caching for product "%s"', product)
             product.profile(
                 calculation_start_date=settings.PEOPLE_COST_CALCATION_STARTING_POINT,
                 ignore_cache=True
             )
+            for start, end in [
+                    (product.discovery_date, product.alpha_date),
+                    (product.alpha_date, product.beta_date),
+                    (product.beta_date, product.live_date),
+                    (product.live_date, product.end_date)
+            ]:
+                if start and end:
+                    product.stats_between(
+                        start,
+                        end,
+                        calculation_start_date=settings.PEOPLE_COST_CALCATION_STARTING_POINT,
+                        ignore_cache=True
+                    )
+                product.current_fte(
+                    start_date=start,
+                    end_date=end,
+                    ignore_cache=True
+                )
 
     def remove(self):
         """
@@ -40,6 +61,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if options['action'] == 'gen':
-            self.generate()
+            products = Product.objects.visible()
+            if options['products']:
+                products = products.filter(contains_any('name', options['products']))
+                if not products:
+                    logging.info('no product found for name %s', options['products'])
+
+            self.generate(products)
         elif options['action'] == 'rm':
             self.remove()
